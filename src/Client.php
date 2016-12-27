7f3aa726ee1fe8d54ca4ff60b43f303e3287d97e
Manuel Mendoza <alejobit@gmail.com>
2016-12-27 17:27:03 -0400

    Fix wsdl namespace and add classmap client option

--- src/Client.php
+++ src/Client.php
@@ -17,15 +17,30 @@ class Client extends \SoapClient
      * @param string $tranKey
      * @param string $wsdl
      */
-    public function __construct($login, $tranKey, $wsdl = 'https://test.placetopay.com/soap/pse?wsdl')
+    public function __construct($login, $tranKey, $wsdl = 'https://test.placetopay.com/soap/pse/?wsdl')
     {
         $this->auth = new Authentication($login, $tranKey);
 
-        parent::__construct($wsdl);
+        parent::__construct($wsdl, array(
+            'classmap' => $this->getClassMap(),
+        ));
     }
 
     public function getBankList()
     {
         return parent::getBankList(new GetBankList($this->auth));
+    }
+
+    private function getClassMap()
+    {
+        return array(
+            'Attribute' => '\\PlacetoPay\\PSE\\Struct\\Attribute',
+            'ArrayOfAttribute' => '\\PlacetoPay\\PSE\\Struct\\ArrayOfAttribute',
+            'Authentication' => '\\PlacetoPay\\PSE\\Struct\\Authentication',
+            'getBankList' => '\\PlacetoPay\\PSE\\Struct\\GetBankList',
+            'Bank' => '\\PlacetoPay\\PSE\\Struct\\Bank',
+            'ArrayOfBank' => '\\PlacetoPay\\PSE\\Struct\\ArrayOfBank',
+            'getBankListResponse' => '\\PlacetoPay\\PSE\\Struct\\GetBankListResponse',
+        );
     }
 }
